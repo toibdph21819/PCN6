cb228850fde57533130dd82309b8aa82492e7a52
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-09-23 21:24:12 +0700

    cart

---
+++ components/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <div>
+    <div class="logo">logo</div>
+    <div class="nav-link">
+      <div>Hỗ trợ</div>
+      <div><a href="./index.php">phản hồi</a></div>
+    </div>
+    <div class="nav-link">
+      <div>các trang</div>
+      <div><a href="./index.php">Trang chủ</a></div>
+      <div><a href="./index.php">sản phẩm</a></div>
+      <div><a href="./index.php">Giỏ hàng</a></div>
+    </div>
+    <div class="nav-link">
+      <div>Tài khoản</div>
+      <div><a href="./index.php">Đăng nhập</a></div>
+      <div><a href="./index.php">Đăng ký</a></div>
+    </div>
+  </div>
+</footer>
+</div>
+</body>
+
+</html>
---
+++ components/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="global.css" />
+</head>
+
+<body>
+  <div class="wrap">
+    <header>
+      <div>
+        <div class="logo">LOGO</div>
+        <div class="search">
+          <form action="">
+            <input type="text" name="search">
+            <button class="btn "> <i class="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+        </div>
+        <nav>
+          <a href="./index.php">Trang chủ</a>
+          <a href="./index.php">Sản phẩm</a>
+          <a href="./index.php">Giỏ hàng</a>
+          <a href="./index.php">phản hồi</a>
+          <a href="./index.php">Đăng ký</a>
+          <a href="./index.php">Đăng nhập</a>
+        </nav>
+      </div>
+    </header>
