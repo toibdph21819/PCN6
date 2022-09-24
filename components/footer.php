6ca6ab8a7e28104062c258660cf076d1b83b6173
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-09-24 19:07:20 +0700

    a

--- components/footer.php
+++ components/footer.php
@@ -1,17 +1,18 @@
-<footer>
-  <div>
-    <div class="logo">logo</div>
-    <div class="nav-link">
+</div>
+<footer class="bg-primary ">
+  <div class="w-[1060px]  max-w-full mx-auto flex h-52 mt-52 justify-between items-center">
+    <div class="text-3xl">LOGO</div>
+    <div class="text-white text-center -translate-y-5">
       <div>Hỗ trợ</div>
       <div><a href="./index.php">phản hồi</a></div>
     </div>
-    <div class="nav-link">
+    <div class="text-white text-center">
       <div>các trang</div>
       <div><a href="./index.php">Trang chủ</a></div>
       <div><a href="./index.php">sản phẩm</a></div>
       <div><a href="./index.php">Giỏ hàng</a></div>
     </div>
-    <div class="nav-link">
+    <div class="text-white text-center -translate-y-4">
       <div>Tài khoản</div>
       <div><a href="./index.php">Đăng nhập</a></div>
       <div><a href="./index.php">Đăng ký</a></div>
--- components/header.php
+++ components/header.php
@@ -7,27 +7,40 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="global.css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#cf9d1c',
+          }
+        }
+      }
+    }
+  </script>
 </head>
 
-<body>
-  <div class="wrap">
-    <header>
-      <div>
-        <div class="logo">LOGO</div>
-        <div class="search">
-          <form action="">
-            <input type="text" name="search">
-            <button class="btn "> <i class="fa-solid fa-magnifying-glass"></i></button>
-          </form>
+<body class="capitalize bg-[#f1f1f1]">
+  <div class="">
+    <header class="bg-primary  top-0 left-0 right-0 fixed ">
+      <div class=" ">
+        <div class="mx-auto w-[1060px] flex h-24 justify-between items-center">
+          <div class="text-3xl">LOGO</div>
+          <div class="">
+            <form action="" class="relative">
+              <input class="h-9 rounded-l-2xl border-0 w-64 outline-none px-3 text-sm" type="text" name="search">
+              <button class="h-9 absolute rounded-r-2xl bg-white px-2"> <i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+          </div>
+          <nav>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">Trang chủ</a>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">Sản phẩm</a>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">Giỏ hàng</a>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">phản hồi</a>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">Đăng ký</a>
+            <a class="text-white px-2 py-1 hover:text-primary hover:bg-white" href="./index.php">Đăng nhập</a>
+          </nav>
         </div>
-        <nav>
-          <a href="./index.php">Trang chủ</a>
-          <a href="./index.php">Sản phẩm</a>
-          <a href="./index.php">Giỏ hàng</a>
-          <a href="./index.php">phản hồi</a>
-          <a href="./index.php">Đăng ký</a>
-          <a href="./index.php">Đăng nhập</a>
-        </nav>
       </div>
     </header>
