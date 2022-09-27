086747f195a5ab478aae7139e26bd0ca5ee059c2
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-09-27 15:20:57 +0700

    abc

---
+++ phanhoi.php
@@ -0,0 +1,28 @@
+<?php include_once './components/header.php' ?>
+<div>
+    <div class="rounded-lg w-[700px] mx-auto p-5  bg-[#3888E7]" >
+        <h2 class="text-2xl">Phản hồi</h2>
+        <div class="my-8">
+          <label class=" block text-gray-700  mb-3" for="username">
+            Tên
+          </label>
+          <input class=" rounded-lg shadow appearance-none border w-full h-12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
+        </div>
+        <div class="my-8">
+          <label class="block text-gray-700  mb-3" for="username">
+            Email
+          </label>
+          <input class="shadow appearance-none border rounded w-full h-12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
+        </div>
+        <div class="my-8">
+          <label class="block text-gray-700  mb-3" for="username">
+            Phản hồi
+          </label>
+          <textarea class="w-full rounded-lg " name="" id="" cols="30" rows="6"></textarea>
+        </div>
+        <div class="rounded-lg my-8 w-80 h-14 mx-auto bg-primary">
+            <button class="w-full h-full" type="submit">Gửi</button>
+        </div>
+    </div>
+</div>
+<?php include_once './components/footer.php' ?>
