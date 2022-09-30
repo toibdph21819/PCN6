8309e7691f7cbb4b094a84e56c30f15d5c2c73a9
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-09-30 14:01:02 +0700

    a

--- components/products.php
+++
@@ -1,7 +0,0 @@
-<div class="grid grid-cols-5 gap-2.5">
-  <?php include './components/product.php' ?>
-  <?php include './components/product.php' ?>
-  <?php include './components/product.php' ?>
-  <?php include './components/product.php' ?>
-  <?php include './components/product.php' ?>
-</div>
