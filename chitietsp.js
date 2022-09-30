4b3ef25dc94e799037ed549797289eaad5fce7ed
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-09-30 13:44:52 +0700

    thêm đánh giá ngôi sao

--- chitietsp.js
+++ chitietsp.js
@@ -1,7 +1,9 @@
 const prevNumberProduct = document.querySelector('.prevNumberProduct')
 const nextNumberProduct = document.querySelector('.nextNumberProduct')
 let inputNumberProduct = document.querySelector('.inputNumberProduct')
-
+const createComment = document.querySelector('.createComment')
+const showCreateComment = document.querySelector('.showCreateComment')
+const hideCreateComment = document.querySelector('.hideCreateComment')
 prevNumberProduct.onclick = () => {
   if (inputNumberProduct.value <= 1) {
     prevNumberProduct.disabled = true;
@@ -28,4 +30,12 @@ inputNumberProduct.oninput = (e) => {
   else if (inputNumberProduct.value >= 1) {
     prevNumberProduct.disabled = false;
   }
+}
+showCreateComment.onclick = function () {
+  if (createComment.classList.contains('hidden')) {
+    createComment.classList.remove('hidden')
+  }
+}
+hideCreateComment.onclick = function () {
+  createComment.classList.add('hidden');
 }
