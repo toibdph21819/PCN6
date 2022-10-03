b02d4106ced196ef8b3dbea7c3c88a94282b78c2
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-03 13:42:39 +0700

    thêm trang thanh toán

--- site/thanhtoan/thanhtoan.php
+++ site/thanhtoan/thanhtoan.php
@@ -9,19 +9,60 @@
         <div class="space-x-3 text-xs"><span class="font-bold">Văn A</span><span class="font-bold">0312344983</span><span>Số nhà 2 ngõ 11 phố hồ chí minh phường abc</span><a href="" class="text-primary underline">Thay đổi</a></div>
       </div>
     </div>
-    <div class=" p-5 bg-white ">
-      <table class="w-full">
+    <div class=" p-5 bg-white mb-5">
+      <table class=" w-full">
         <tr class="text-sm">
-          <td colspan="3">Sản phẩm</td>
-          <td>đơn giá</td>
-          <td>số lượng</td>
-          <td>thành tiền</td>
+          <td colspan="3" class=" p-2 text-lg">Sản phẩm</td>
+          <td class=" p-2 text-center">đơn giá</td>
+          <td class=" p-2 text-center">số lượng</td>
+          <td class=" p-2 text-center">thành tiền</td>
         </tr>
         <tr class="text-xs">
-          <td><img src="<?= $CONTENT_URL ?>/images/a1.jpg" class="h-8 w-8" alt=""></td>
-          <td class="w-80 overflow-hidden whitespace-nowrap text-ellipsis">fffffffffffffffffffffffffffffff fTiêu đềddddd fffffffffffdddddddddddddddddddddđ</td>
+          <td class="p-2"><img src="<?= $CONTENT_URL ?>/images/a1.jpg" class="h-8 w-8" alt=""></td>
+          <td class="max-w-[360px]">
+            <p class=" max-w-[340px] overflow-hidden whitespace-nowrap text-ellipsis p-2">poidu jdiij doep djeomx kdok poidu jdiij doep djeomx kdokpoidu jdiij doep djeomx kdok ff</p>
+          </td>
+          <td class=" p-2 text-center">loại: ram 8 gb</td>
+          <td class=" p-2 text-center">10.000</td>
+          <td class=" p-2 text-center">1</td>
+          <td class=" p-2 text-center">10.000</td>
         </tr>
       </table>
+      <div class="flex mt-5 bg-[#fafdff] border border-dashed p-5">
+        <div class="w-1/2 flex items-baseline gap-x-4">
+          <label for="">Lời nhắn:</label>
+          <input class="shadow appearance-none border rounded w-64 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Lưu ý cho người bán" id="" type="text">
+        </div>
+      </div>
+    </div>
+    <div class="p-5 bg-white flex justify-between border-b">
+      <h2>Phương thức thanh toán</h2>
+      <div class="text-xs">
+        <div>Thanh toán khi nhận hàng</div>
+      </div>
+    </div>
+    <div class="p-5 bg-white flex justify-between border-b">
+      <div class="w-1/2"></div>
+      <div class="w-1/3 text-xs text-gray-500">
+        <div class="flex w-full items-center justify-between">
+          <p>Tổng tiền hàng</p>
+          <p>99999999</p>
+        </div>
+        <div class="flex w-full items-center justify-between">
+          <p>phí vận chuyển </p>
+          <p>99999999</p>
+        </div>
+        <div class="flex w-full items-center justify-between">
+          <p>Tổng thanh toán</p>
+          <p class="text-primary text-lg">99999999</p>
+        </div>
+
+
+      </div>
+    </div>
+    <div class="p-5 flex bg-white items-center justify-between">
+      <p>Nhấn đặt hàng đồng nghĩa với việc bạn chấp nhận điều khoản của PCN6</p>
+      <div><button class="text-white bg-primary rounded-md px-10 py-5">Đặt hàng</button></div>
     </div>
   </div>
 </div>
