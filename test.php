2925caf81f09789c0e7e2baffa9f67c703606aee
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-03 09:17:06 +0700

    thử các dao

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php
 require './dao/pdo.php';
-require './dao/loai.php';
-require './dao/thuonghieu.php';
-require './dao/sanpham.php';
+require './dao/products.php';
+// require './dao/thuonghieu.php';
+// require './dao/sanpham.php';
+// product_insert('abc', 1234, 3, 1, 1, 'abđhhhhh', 2, 1, 3);
+$por = product_select_all();
+var_dump($por);
