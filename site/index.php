6b26e97ae2408e5ed597a5aef706e1de4be4ce8e
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-05 11:19:57 +0700

    thêm các đường dẫn đến các trang

--- site/index.php
+++ site/index.php
@@ -2,12 +2,20 @@
 require_once '../global.php';
 if (isset($_GET['giohang'])) {
 
-  $VIEW_NAME = 'giohang.php';
+  $VIEW_NAME = 'gio_hang.php';
 } elseif (isset($_GET['phanhoi'])) {
 
   $VIEW_NAME = 'phanhoi.php';
-} else {
+} elseif (isset($_GET['dangky'])) {
 
+  $VIEW_NAME = 'dangky.php';
+} elseif (isset($_GET['dangnhap'])) {
+
+  $VIEW_NAME = 'dangnhap.php';
+} elseif (isset($_GET['taikhoan'])) {
+
+  $VIEW_NAME = 'tai_khoan.php';
+} else {
   $VIEW_NAME = 'home.php';
 }
 
