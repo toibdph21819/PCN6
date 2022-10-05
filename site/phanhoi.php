76361aa373d541533b7db7cdf7e307bd177bfe47
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-05 11:20:27 +0700

    cập nhật trang phản hồi

--- site/phanhoi.php
+++ site/phanhoi.php
@@ -1,26 +1,36 @@
-<div>
-  <div class="rounded-lg w-[700px] mx-auto p-5  bg-[#3888E7]">
-    <h2 class="text-2xl">Phản hồi</h2>
-    <div class="my-8">
-      <label class=" block text-gray-700  mb-3" for="username">
-        Tên
-      </label>
-      <input class=" rounded-lg shadow appearance-none border w-full h-12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
-    </div>
-    <div class="my-8">
-      <label class="block text-gray-700  mb-3" for="username">
-        Email
-      </label>
-      <input class="shadow appearance-none border rounded w-full h-12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
-    </div>
-    <div class="my-8">
-      <label class="block text-gray-700  mb-3" for="username">
-        Phản hồi
-      </label>
-      <textarea class="w-full rounded-lg " name="" id="" cols="30" rows="6"></textarea>
-    </div>
-    <div class="rounded-lg my-8 w-80 h-14 mx-auto bg-primary">
-      <button class="w-full h-full" type="submit">Gửi</button>
+<div class="w-full px-5 mx-auto flex  justify-center py-5 bg-slate-700">
+  <div class="p-5">
+    <h2 class="text-2xl text-center mb-10 text-white">đưa ra các ý kiến để chúng tôi có thể tốt hơn</h2>
+    <div class="shadow-md p-7 rounded-md bg-white">
+      <div class="text-center mb-5">
+        <b class="text-xl">phản hồi</b>
+      </div>
+      <form class="" method="post" action="">
+        <div class="mt-4 space-y-3">
+          <label for="">email</label>
+          <input type="text" name="email" placeholder="Nhập email" class="p-2 border shadow-sm rounded-md h-10 w-full">
+        </div>
+        <div class="mt-4 space-y-3">
+          <label for="">tên</label>
+          <input type="text" name="name" placeholder="Nhập tên" class="p-2 border shadow-sm rounded-md h-10 w-full">
+        </div>
+
+        <div class="mt-4 space-y-3">
+          <label for="">ý kiến của bạn</label>
+          <textarea name="" id="" cols="" rows="3" class="p-2 border shadow-sm rounded-md  w-full"></textarea>
+        </div>
+
+
+
+        <div class="text-center w-full  mx-auto mt-5">
+          <input type="submit" value="Đăng nhập" class="transition-colors w-full py-2 rounded-md bg-primary hover:bg-transparent cursor-pointer hover:text-black text-white border-primary border">
+        </div>
+        <div class="text-center w-full  mx-auto mt-5">
+          <a href="<?= $SITE_URL ?>" class="transition-colors block w-full py-2 rounded-md border-primary border hover:bg-primary hover:text-white">Trở về</a>
+        </div>
+
+      </form>
     </div>
   </div>
+
 </div>
