22d4e021997a2afb65dd971da087835ef26295c1
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-05 14:41:03 +0700

    tạo phần sidebar

---
+++ admin/layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/output.css">
+</head>
+
+<body>
+  <div class="flex">
+    <div class="w-1/6">
+      <nav>
+        <div>
+          <h2 class="text-white">logo</h2>
+          <button class="border-none outline-none bg-white"><i class="fa-solid fa-bars"></i></button>
+        </div>
+
+      </nav>
+    </div>
+  </div>
+</body>
+
+</html>
