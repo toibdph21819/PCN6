e449d4dad9cb60a68424aa20c2f5419f71f63d0d
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-09 15:18:45 +0700

    thêm đường dẫn

--- global.php
+++ global.php
@@ -3,8 +3,3 @@ $ROOT_URL = "/PCN6";
 $CONTENT_URL = "$ROOT_URL/content";
 $ADMIN_URL = "$ROOT_URL/admin";
 $SITE_URL = "$ROOT_URL/site";
-
-function exist_param($fieldname)
-{
-  return array_key_exists($fieldname, $_REQUEST);
-}
