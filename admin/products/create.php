d10de8c73fa5488f1ce94e9890876f0a1693408a
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-10 10:02:28 +0700

    thêm các trường cần thiết

--- admin/products/create.php
+++ admin/products/create.php
@@ -1,17 +1,10 @@
 <div class="bg-white p-5 min-h-[100vh] flex items-center">
-  <form name="form" onsubmit="return validate()" class="w-full  mx-auto p-5 rounded-md  border" enctype="multipart/form-data" action="index.php?insert" method="post">
+  <form name="form" onsubmit="return validate()" class="w-full  mx-auto p-5 rounded-md  border" enctype="multipart/form-data" action="index.php?create" method="post">
     <div class="flex space-x-2 justify-end">
       <a href="index.php" type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Trở về</a>
     </div>
     <h2 class="text-xl text-center my-5">Thêm mới sản phẩm</h2>
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
 
-        <input value="<?= $get_id ?>" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" type="text" name="product_id" placeholder="">
-        <p class=" text-xs italic error text-red-600"></p>
-
-      </div>
-    </div>
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full px-3">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="">
@@ -19,7 +12,7 @@
         </label>
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" type="text" name="name" placeholder="">
         <p class="text-gray-600 text-xs italic">Tuỳ bạn đặt như nào thì đặt</p>
-        <p class=" text-xs italic error text-red-600"></p>
+        <p class=" text-xs italic error text-red-600"><?= $err['name'] ?? '' ?></p>
 
       </div>
     </div>
@@ -30,6 +23,8 @@
         </label>
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="images" type="file" name="images[]" multiple>
         <p class="text-gray-600 text-xs italic">có thể chọn nhiều hình ok</p>
+        <p class=" text-xs italic error text-red-600"><?= $err['image'] ?? '' ?></p>
+
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6 space-x-3">
@@ -40,7 +35,7 @@
         <label class="form-check-label inline-block text-gray-800" for="featured_ok">
           Chuẩn rồi
         </label>
-        <p class="text-red-500 text-xs italic"><?= $err['featured'] ?? '' ?></p>
+
 
       </div>
       <div class="form-check space-x-1">
@@ -49,6 +44,8 @@
           không phải nhé
         </label>
       </div>
+      <p class=" text-xs italic error text-red-600"><?= $err['featured'] ?? '' ?></p>
+
     </div>
     <div class="flex flex-wrap -mx-3 mb-6 space-x-3">
       <p class="w-full ml-3">Trạng thái?</p>
@@ -64,39 +61,19 @@
           không
         </label>
       </div>
+      <p class=" text-xs italic error text-red-600"><?= $err['active'] ?? '' ?></p>
+
     </div>
 
 
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full px-3">
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-          Thêm loại
-        </label>
-        <div class="relative">
-          <?php if (count($rows_types) == 0) : ?>
-            <h3 class="text-xl">Không có dữ liệu</h3>
-          <?php else : ?>
-            <select name="type_id[]" multiple class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
 
-              <?php foreach ($rows_types as $type) : ?>
-                <option value="<?= $type['id'] ?>">
-                  <?= $type['name'] ?>
-                </option>
 
-              <?php endforeach; ?>
-            </select>
-          <?php endif ?>
-        </div>
-        <p class="text-gray-600 text-xs italic">có thể chọn nhiều ok</p>
-
-      </div>
-    </div>
     <div class="flex flex-wrap -mx-3 mb-2">
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
           giá
         </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="" type="number" name="price" placeholder="">
+        <input value="0" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="" type="number" name="price" placeholder="">
       </div>
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
@@ -119,6 +96,7 @@
               <i class="fa-solid fa-caret-down"></i>
             </div>
           <?php endif ?>
+          <p class=" text-xs italic error text-red-600"><?= $err['brand_id'] ?? '' ?></p>
         </div>
       </div>
     </div>
@@ -144,6 +122,7 @@
               <i class="fa-solid fa-caret-down"></i>
             </div>
           <?php endif ?>
+          <p class=" text-xs italic error text-red-600"><?= $err['brand_id'] ?? '' ?></p>
         </div>
       </div>
       <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -167,6 +146,8 @@
               <i class="fa-solid fa-caret-down"></i>
             </div>
           <?php endif ?>
+          <p class=" text-xs italic error text-red-600"><?= $err['brand_id'] ?? '' ?></p>
+
         </div>
       </div>
     </div>
@@ -176,8 +157,6 @@
           mô tả
         </label>
         <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="" type="text" name="description" placeholder=""></textarea>
-        <p class=" text-xs italic error text-red-600"></p>
-
       </div>
     </div>
     <div class="flex space-x-2 justify-center mt-4">
@@ -185,9 +164,27 @@
     </div>
   </form>
 </div>
-<script>
+<!-- <script>
+  var openBlock = () => {
+    var open_block = document.querySelector('.open_block');
+    if (open_block.classList.contains('block')) {
+      open_block.classList.remove('block');
+      open_block.classList.add('hidden');
+
+    } else {
+      open_block.classList.add('block');
+      open_block.classList.remove('hidden');
+    }
+  }
   var validate = () => {
     let name = document.querySelector('input[name="name"]');
+    let slt = document.querySelectorAll('select');
+    slt.foreach(item => {
+      if (item.value == "") {
+        alert('Please enter first name');
+        return false;
+      }
+    })
     if (name.value == '') {
       var err = name.parentElement;
       err = err.querySelector('.error');
@@ -198,5 +195,24 @@
       err = err.querySelector('.error');
       err.innerHTML = '';
     }
+    const images = document.getElementById('images');
+    if (!images.value) {
+      alert('No images found');
+      return false;
+    }
+    for (var i = 0; i < images.files.length; i++) {
+      var f = images.files[i];
+      if (!endsWith(f.name, 'jpg') && !endsWith(f.name, 'png')) {
+        alert(f.name + " is not a valid file!");
+        return false;
+      } else {
+        return true;
+      }
+    }
+
   }
-</script>
+
+  function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  }
+</script> -->
