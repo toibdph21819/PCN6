ffbb38afb4f3437111925328b49b9ac80f2f7ac8
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-10 10:53:53 +0700

    thêm các hàm về cookie

--- global.php
+++ global.php
@@ -3,3 +3,16 @@ $ROOT_URL = "/PCN6";
 $CONTENT_URL = "$ROOT_URL/content";
 $ADMIN_URL = "$ROOT_URL/admin";
 $SITE_URL = "$ROOT_URL/site";
+
+function add_cookie($name, $value, $day)
+{
+  setcookie($name, $value, time() + (86400 + $day), "/");
+}
+function delete_cookie($name)
+{
+  add_cookie($name, "", -1);
+}
+function get_cookie($name)
+{
+  return $_COOKIE[$name] ?? '';
+}
