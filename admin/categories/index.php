7b925cb9433a35485c41089a4eabc6dcb139d89c
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-11 16:22:53 +0700

    thêm sửa xoá danh mục

---
+++ admin/categories/index.php
@@ -0,0 +1,84 @@
+<?php
+require_once '../../global.php';
+require_once '../../dao/pdo.php';
+
+require_once '../../dao/categories.php';
+if (isset($_GET['create'])) {
+  if (isset(($_POST['submit']))) {
+    //bình thường
+    $name = $_POST['name'];
+    $image = $_FILES['image'];
+    $err = [];
+    if ($name == '') {
+      $err['name'] = "Cần có dữ liệu";
+    }
+    if ($image['size'] == 0) {
+      $err['image'] = "Cần có hình";
+    } elseif ($image['size'] > 2 * 1024 * 1024) {
+      $err['image'] = "Hình đã lớn hơn 2mb";
+    } elseif ($image['size'] > 0 && $image['size'] <= 2 * 1024 * 1024) {
+
+      $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+      $ext = strtolower($ext);
+      if ($ext == 'jpg' || $ext == 'png') {
+      } else {
+        $err['image'] = "không đúng định dạng";
+      }
+    }
+
+    if ($err == []) {
+      categories_insert($name, $image['name']);
+      move_uploaded_file($image['tmp_name'], "../../content/images/" . $image['name']);
+      header("location: " . $ADMIN_URL . '/categories/index.php');
+      die;
+    }
+  }
+
+  $VIEW_NAME = 'create.php';
+} elseif (isset($_GET['edit'])) {
+  $get_id = $_GET['id'] ?? "";
+  if (isset(($_POST['submit']))) {
+    //bình thường
+    $name = $_POST['name'];
+    $image = $_FILES['image'];
+    $prev_img = $_POST['prev_img'];
+    $err = [];
+    if ($name == '') {
+      $err['name'] = "Cần có dữ liệu";
+    }
+    if ($image['size'] > 2 * 1024 * 1024) {
+      $err['image'] = "Hình đã lớn hơn 2mb";
+    } elseif ($image['size'] > 0 && $image['size'] <= 2 * 1024 * 1024) {
+
+      $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+      $ext = strtolower($ext);
+      if ($ext == 'jpg' || $ext == 'png') {
+      } else {
+        $err['image'] = "không đúng định dạng";
+      }
+    }
+
+    if ($err == []) {
+      if ($image['size'] == 0) {
+        $image['name'] = $prev_img;
+      }
+      categories_update($get_id, $name, $image['name']);
+      if ($image['size'] > 0) {
+        move_uploaded_file($image['tmp_name'], "../../content/images/" . $image['name']);
+      }
+      header("location: " . $ADMIN_URL . '/categories/index.php');
+      die;
+    }
+  }
+  $row =  categories_select_by_id($get_id);
+  $VIEW_NAME = 'edit.php';
+} elseif (isset($_GET['delete'])) {
+  $get_id = $_GET['id'] ?? "";
+  categories_delete($get_id);
+  header("location: " . $ADMIN_URL . '/categories/index.php');
+  die;
+} else {
+  $rows = categories_select_all();
+  $VIEW_NAME = 'list.php';
+}
+include_once '../layout.php';
---
+++ admin/categories/list.php
@@ -0,0 +1,54 @@
+<div class="bg-white flex justify-center">
+  <div class="flex flex-col p-10">
+    <h2 class="text-xl text-center">Danh sách danh mục</h2>
+    <div>
+      <div class="flex space-x-2 justify-center">
+        <a href="index.php?create" type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Thêm mới</a>
+      </div>
+    </div>
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+
+        <div class="overflow-hidden">
+          <table class="min-w-full border text-center">
+            <thead class="border-b">
+              <tr>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                  #
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                  tên
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                  hình ảnh
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 border-r py-4">
+                  Thao tác
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php if (count($rows) == 0) : ?>
+                <p>Chưa có dữ liệu</p>
+              <?php else : ?>
+                <?php foreach ($rows as $row) : ?>
+                  <tr class="border-b">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r"><?= $row['id'] ?? "" ?></td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                      <?= $row['name'] ?? "" ?>
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                      <img class="h-12 w-full object-contain" src="<?= $CONTENT_URL ?>/images/<?= $row['image'] ?? "" ?>" alt="">
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r space-x-2"><a href="index.php?edit&id=<?= $row['id'] ?? "" ?>" class="underline text-primary">sửa</a><a href="index.php?delete&id=<?= $row['id'] ?? "" ?>" class="underline text-primary" onclick="return confirm('Chắc chưa')">xoá</a></td>
+
+                  </tr>
+                <?php endforeach; ?>
+              <?php endif; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
