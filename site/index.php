613c186fcc7a0fe5cc03e6b1bdae333db8caa8d5
thanh23896 <thanhnhph23896@fpt.edu.vn>
2022-10-14 14:14:27 +0700

    lấy dữ liệu cho tài khoản

--- site/index.php
+++ site/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../global.php';
 require_once '../dao/pdo.php';
+require_once '../dao/users.php';
 
 if (isset($_GET['giohang'])) {
 
@@ -23,7 +24,6 @@ if (isset($_GET['giohang'])) {
 
   $VIEW_NAME = 'dangky.php';
 } elseif (isset($_GET['dangnhap'])) {
-  require_once '../dao/users.php';
   if (isset($_POST['submit'])) {
     $err = [];
     $password = $_POST['password'];
@@ -49,7 +49,41 @@ if (isset($_GET['giohang'])) {
 
   $VIEW_NAME = 'dangnhap.php';
 } elseif (isset($_GET['taikhoan'])) {
+  $id = get_cookie('user_id');
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $avatar = $_FILES['avatar'];
+    $avatar_name = $avatar['name'];
+    $prev_img = $_POST['prev_img'];
+    $err = [];
+    if ($email == '') {
+      $err['email'] = "Cần có dữ liệu";
+    }
+    if ($avatar['size'] > 2 * 1024 * 1024) {
+      $err['avatar'] = "Hình đã lớn hơn 2mb";
+    } elseif ($avatar['size'] > 0 && $avatar['size'] <= 2 * 1024 * 1024) {
 
+      $ext = pathinfo($avatar['name'], PATHINFO_EXTENSION);
+      $ext = strtolower($ext);
+      if ($ext == 'jpg' || $ext == 'png') {
+      } else {
+        $err['avatar'] = "không đúng định dạng";
+      }
+    }
+
+    if ($err == []) {
+      if ($avatar['size'] == 0) {
+        $avatar['name'] = $prev_img;
+      }
+      users_update($id, $name, $email, $avatar['name'], $phone);
+      if ($avatar['size'] > 0) {
+        move_uploaded_file($avatar['tmp_name'], "../content/images/" . $avatar['name']);
+      }
+    }
+  }
+  $row = users_select_by_id($id);
   $VIEW_NAME = 'tai_khoan.php';
 } elseif (isset($_GET['dangxuat'])) {
   delete_cookie('user_id');
